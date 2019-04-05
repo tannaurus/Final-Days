911445ec5d9185a9add52efb6b799bc0098c30d0
tannaurus <contacttannergill@gmail.com>
2019-04-04 20:30:25 -0700

    Created simple player movement script with mouse input directional turning

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 2f;
+    private Rigidbody player;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        // Grab the Rigidbody and set it as the "player"
+        player = GetComponent<Rigidbody>();
+        // Locate the camera object
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePlayerDirectionalMovement();
+        UpdatePlayerRotation();
+    }
+
+    void FixedUpdate()
+    {
+        // Update the player's movement
+        player.velocity = moveVelocity;
+    }
+
+    void UpdatePlayerDirectionalMovement()
+    {
+        // ** -- MOVEMENT -- **
+        // Grab the input of the player, leaving the Y value as 0f
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        // Multiply that value by our speed
+        moveVelocity = moveInput * movementSpeed;
+    }
+
+    void UpdatePlayerRotation()
+    {
+        // ** -- ROTATION -- **
+        // Create a ray from the camera's position to the mouse position
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        // Create a mathematical ray for the camera's ray to hit
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        // Store the length of the ray
+        float rayLength;
+        // The rayLength value is defined below, the distance from the groundPlane and the camera
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            // Create a vector 3 based on the point in which the camera's ray intersects with the plane
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            // Fix the player's y position so they don't rotate down/up
+            pointToLook.y = transform.position.y;
+            // Set the player's transform to look at the intersect
+            transform.LookAt(pointToLook);
+        }
+    }
+}
