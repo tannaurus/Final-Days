dac3475dfd60d855cd23d58fc7ef8c1ca4c4eb9b
tannaurus <contacttannergill@gmail.com>
2019-04-06 17:15:43 -0700

    Health script now supports two different skins for when the game object is in a 'damaged' or 'default' state

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Health : MonoBehaviour {
 
+    public Material defaultSkin;
+    public Material damagedSkin;
+    public float damageTime = 0.2f;
+
     public float health = 100f;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        UpdateMaterialTo(defaultSkin);
     }
 
     void GetThrown(MeleeAttack.Throw attackThrow)
@@ -21,9 +26,26 @@ public class Health : MonoBehaviour {
     void TakeDamage(float damage)
     {
         health -= damage;
+        StartCoroutine(FlashMaterial());
         if (health <= 0f)
         {
             Destroy(gameObject);
         }
+    }
+
+    void UpdateMaterialTo(Material mat)
+    {
+        Renderer[] childrenRenderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in childrenRenderers)
+        {
+            renderer.sharedMaterial = mat;
+        }
+    }
+
+    IEnumerator FlashMaterial()
+    {
+        UpdateMaterialTo(damagedSkin);
+        yield return new WaitForSeconds(damageTime);
+        UpdateMaterialTo(defaultSkin);
     }
 }
