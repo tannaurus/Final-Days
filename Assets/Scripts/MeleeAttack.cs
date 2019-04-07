4e6730954854252915be3a9fad2141bced82e230
tannaurus <contacttannergill@gmail.com>
2019-04-07 16:14:13 -0700

    Refactored the 'attack' method to be more re-usable

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -45,12 +45,13 @@ public class MeleeAttack : MonoBehaviour {
         }
     }
 
-    void LocateEnemyInWeaponRange()
+    List<Collider> LocateEnemyInWeaponRange()
     {
         // Grab height of the game object. This will help us scan for objects "in front of us"
         float objectHeight = transform.lossyScale.y;
         // Find any colliders within the weapon's range. Not that, for the starting position, we're starting from the height's middle point.  
         Collider[] hitColliders = Physics.OverlapSphere(new Vector3(transform.position.x, transform.position.y + (objectHeight / 2), transform.position.z), weapon.range);
+        List<Collider> enemies = new List<Collider>();
         int i = 0;
         while (i < hitColliders.Length)
         {
@@ -58,25 +59,13 @@ public class MeleeAttack : MonoBehaviour {
             // Check to ensure we're interacting with only the tags we want to interact with.
             if (col.tag == "Infected")
             {
-                // Give the Infected weapon damage.
-                col.SendMessage("TakeDamage", weapon.damage);
-                // Create a value that will act as the force given to the enemy being thrown in the direction of the attack.
-                // The value that "damage" is multiplied is arbitrary to achieve the desired effect. 
-                float throwForce = weapon.damage * 150;
-                // Do the math to calculate the angle in which the target is from the enemy.
-                Vector3 directionToTarget = col.transform.position - transform.position;
-                // Normalize that Vector3 to get to direction only.
-                directionToTarget.Normalize();
-                // Create a new Throw class, giving it our two values.
-                Throw attackThrow = new Throw(directionToTarget, throwForce);
-                // Give both of those values to the "GetThrown" in order to throw the enemy backware
-                col.SendMessage("GetThrown", attackThrow);
+                // Add this collider to our array of enemies
+                enemies.Add(col);
             }
-            // Create a buffer between attacks
-            attackCooldown = Time.time + weapon.speed;
             // Make sure we don't get stuck in a loop
             i++;
         }
+        return enemies;
     }
 
     void Attack()
@@ -84,7 +73,27 @@ public class MeleeAttack : MonoBehaviour {
         // Only attack after the cooldown buffer time window has passed.
         if (Time.time > attackCooldown)
         {
-            LocateEnemyInWeaponRange();
+            List<Collider> localEnemies = LocateEnemyInWeaponRange();
+            foreach (Collider enemy in localEnemies) 
+            {
+                // Give the Infected weapon damage.
+                enemy.SendMessage("TakeDamage", weapon.damage);
+                // Create a value that will act as the force given to the enemy being thrown in the direction of the attack.
+                // The value that "damage" is multiplied is arbitrary to achieve the desired effect. 
+                float throwForce = weapon.damage * 150;
+                // Do the math to calculate the angle in which the target is from the enemy.
+                Vector3 directionToTarget = enemy.transform.position - transform.position;
+                // Normalize that Vector3 to get to direction only.
+                directionToTarget.Normalize();
+                // Create a new Throw class, giving it our two values.
+                Throw attackThrow = new Throw(directionToTarget, throwForce);
+                // Give both of those values to the "GetThrown" in order to throw the enemy backware
+                enemy.SendMessage("GetThrown", attackThrow);
+                // Create a buffer between attacks
+                attackCooldown = Time.time + weapon.speed;
+            }
         }
     }
+
+    
 }
