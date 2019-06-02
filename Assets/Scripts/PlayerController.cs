c87e1cc19cb3ba230e52a6214353e169495a2f61
tannaurus <contacttannergill@gmail.com>
2019-06-02 11:18:05 -0700

    Added a replace 3rd person controller from the asset store, replaced player with that. Needs inventory system support.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,6 +52,27 @@ public class PlayerController : MonoBehaviour {
         //moveVelocity = moveInput * movementSpeed;
     }
 
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg * -1;
+    }
+
+    //void UpdatePlayerRotation()
+    //{
+    //    //Get the Screen positions of the object
+    //    Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+    //    //Get the Screen position of the mouse
+    //    Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+    //    //Get the angle between the points
+    //    float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+    //    //Ta Daaa
+    //    transform.rotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+
+    //}
+
     void UpdatePlayerRotation()
     {
         // ** -- ROTATION -- **
