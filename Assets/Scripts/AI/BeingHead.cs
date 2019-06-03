69e2bc6d3133fdd8825417c4620a954477a1a643
tannaurus <contacttannergill@gmail.com>
2019-06-03 13:44:12 -0700

    Commented out the Being Head idle movements code until MVP is hit.

--- Assets/Scripts/AI/BeingHead.cs
+++ Assets/Scripts/AI/BeingHead.cs
@@ -7,13 +7,12 @@ public class BeingHead : MonoBehaviour
     private Transform player;
     public bool awake = true;
     public float viewDistance = 100f;
-    public float speed = 1f;
+    // public float speed = 1f;
 
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        IdleMovement();
     }
 
     // Update is called once per frame
@@ -37,33 +36,33 @@ public class BeingHead : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + transform.forward * viewDistance);
     }
 
-    void IdleMovement()
-    {
-        List<Quaternion> groove = IdleHeadMovements.GetGroove();
-        RunMovement(groove);
-    }
+    //void IdleMovement()
+    //{
+    //    List<Quaternion> groove = IdleHeadMovements.GetGroove();
+    //    RunMovement(groove);
+    //}
 
-    void RunMovement(List<Quaternion> movement, int index = 0)
-    {
-        float calculatedSpeed = speed * Time.deltaTime;
-        Quaternion.RotateTowards(transform.rotation, movement[index], calculatedSpeed);
-        if (index != movement.Count -1)
-        {
-            RunMovement(movement, index + 1);
-        }
-    }
+    //void RunMovement(List<Quaternion> movement, int index = 0)
+    //{
+    //    float calculatedSpeed = speed * Time.deltaTime;
+    //    Quaternion.RotateTowards(transform.rotation, movement[index], calculatedSpeed);
+    //    if (index != movement.Count -1)
+    //    {
+    //        RunMovement(movement, index + 1);
+    //    }
+    //}
 
 }
 
-class IdleHeadMovements
-{
-    public static List<Quaternion> GetGroove()
-    {
-        List<Quaternion> groove = new List<Quaternion>();
-        groove.Add(Quaternion.Euler(0, 0, 0));
-        groove.Add(Quaternion.Euler(50, 0, 0));
-        groove.Add(Quaternion.Euler(0, 0, 0));
-        groove.Add(Quaternion.Euler(-50, 0, 0));
-        return groove;
-    }
-}
+//class IdleHeadMovements
+//{
+//    public static List<Quaternion> GetGroove()
+//    {
+//        List<Quaternion> groove = new List<Quaternion>();
+//        groove.Add(Quaternion.Euler(0, 0, 0));
+//        groove.Add(Quaternion.Euler(50, 0, 0));
+//        groove.Add(Quaternion.Euler(0, 0, 0));
+//        groove.Add(Quaternion.Euler(-50, 0, 0));
+//        return groove;
+//    }
+//}
