7870c7a31fec8598f653dd4a07e5ca56eb20e691
tannaurus <contacttannergill@gmail.com>
2019-06-03 13:39:49 -0700

    Group game objects for cleaner scene, added NavMesh library, implemented BeingBehavior elements

---
+++ Assets/Scripts/AI/BeingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeingBehavior : MonoBehaviour
+{
+    private Transform player;
+    public bool awake = true;
+    public float viewDistance = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (awake)
+        {
+            RotateBody();
+        }
+    }
+
+    void RotateBody()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+    }
+
+}
---
+++ Assets/Scripts/AI/BeingHead.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeingHead : MonoBehaviour
+{
+    private Transform player;
+    public bool awake = true;
+    public float viewDistance = 100f;
+    public float speed = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        IdleMovement();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (awake)
+        {
+            MonitorPlayer();
+        }
+    }
+
+    void MonitorPlayer()
+    {
+        transform.LookAt(player.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * viewDistance);
+    }
+
+    void IdleMovement()
+    {
+        List<Quaternion> groove = IdleHeadMovements.GetGroove();
+        RunMovement(groove);
+    }
+
+    void RunMovement(List<Quaternion> movement, int index = 0)
+    {
+        float calculatedSpeed = speed * Time.deltaTime;
+        Quaternion.RotateTowards(transform.rotation, movement[index], calculatedSpeed);
+        if (index != movement.Count -1)
+        {
+            RunMovement(movement, index + 1);
+        }
+    }
+
+}
+
+class IdleHeadMovements
+{
+    public static List<Quaternion> GetGroove()
+    {
+        List<Quaternion> groove = new List<Quaternion>();
+        groove.Add(Quaternion.Euler(0, 0, 0));
+        groove.Add(Quaternion.Euler(50, 0, 0));
+        groove.Add(Quaternion.Euler(0, 0, 0));
+        groove.Add(Quaternion.Euler(-50, 0, 0));
+        return groove;
+    }
+}
